f921aee449dcd47740077cf1c0b96feeea7c2c48
Tejas <sawanttejasar@gmail.com>
2023-08-02 15:23:54 +0530

    video play, pause and stop functionality added

---
+++ Assets/Scripts/VideoPlaybackControllerScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlaybackControllerScript : MonoBehaviour
+{
+  public VideoPlayer videoPlayer;
+
+  public void PlayVideo(){
+    videoPlayer.Play();
+  }
+
+  public void PauseVideo(){
+    videoPlayer.Pause();
+  }
+
+  public void StopVideo(){
+    videoPlayer.Stop();
+  }
+}
